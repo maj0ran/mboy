1ddf98570e4920d271c645063103252be5fc9ad2
Marian Cichy <m.cichy@pengutronix.de>
2021-04-06 15:41:05 +0200

    mboy: Add common header file

---
+++ include/common.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdint>
+
+#define kB *1024
+
+using u32 = uint32_t;
+using i32 = uint32_t;
+using u16 = uint16_t;
+using i16 = uint16_t;
+using u8 = uint8_t;
+using i8 = uint8_t;
